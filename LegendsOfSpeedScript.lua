5f21b114bff6ab99f14d719c1aa662ea3f1a6e3b
VyrosxC Hub <alexandregomesap202@gmail.com>
2024-11-20 00:23:04 -0300

    LegendsOfSpeedScript.lua

--- LegendsOfSpeedScript.lua
+++ LegendsOfSpeedScript.lua
@@ -5,7 +5,8 @@ local Window = OrionLib:MakeWindow({Name = "VyrosxC | Key System 🔑", HidePrem
 local KeyDatabase = {
     -- Adicione as chaves manualmente com UserID correspondente
     ["VyrosxC-82384"] = 1396119116,  -- @Alexg78909
-    ["VyrosxC-43456"] = 1762542484,  -- @ericklopes16
+    ["VyrosxC-43456"] = 1762542484,  -- @ericklopes16 
+    ["VyrosxC-43824"] = 1980119628,  -- @CRXM_CRXM 
 }
 
 -- Função para verificar se a chave é válida e associada ao jogador
