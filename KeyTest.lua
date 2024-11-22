2deb5fedd30d78fb4a89961368dd4a81f4fdbd12
VyrosxC Hub <alexandregomesap202@gmail.com>
2024-11-21 21:07:41 -0300

    KeyTest.lua

--- KeyTest.lua
+++ KeyTest.lua
@@ -10,7 +10,7 @@ local KeyDatabase = {
     ["VyrosxC-janiszewskiblox"] = 4374535723,  -- @janiszewskiblox (Eu) 
     ["VyrosxC-Premium-94384"] = 1762542484,  -- @ericklopes16 (Erick)
     ["VyrosxC-Premium-81237"] = 5097989830,  -- @FX_BAHIANOOO (Alt Erick)
-    ["VyrosxC-Premium-96934"] = 1980119628,  -- @CRXM_CRXM (Will)
+    ["VyrosxC-Premium-9999"] = 1980119628,  -- @CRXM_CRXM (Will)
     ["M4-Users008"] = 2628834524,  -- @M4N0TUE641 (Matue)
     -- (restante do banco de dados...)
 }
@@ -30,7 +30,7 @@ end
 local function SaveKeyLocally(key)
     local data = { key = key }
     writefile(CONFIG_FILE, game:GetService("HttpService"):JSONEncode(data))
-    print("Key salva localmente:", key)
+    print("Key saved locally:", key)
 end
 
 -- Função para validar key
@@ -55,9 +55,9 @@ local player = game.Players.LocalPlayer  -- Jogador local
 local function ValidateKey(Value)
     if IsKeyValid(Value, player) then
         OrionLib:MakeNotification({
-            Name = "Key Validated",
-            Content = "Key Is Valid And Accepted!",
-            Image = "rbxassetid://4483345998",
+            Name = "Valid Key",
+            Content = "Enjoy!",
+            Image = "rbxassetid://71378523145158",
             Time = 5
         })
 
@@ -66,8 +66,8 @@ local function ValidateKey(Value)
     else
         OrionLib:MakeNotification({
             Name = "Invalid Key",
-            Content = "This Key Is Either Invalid Or Already Used.",
-            Image = "rbxassetid://4483345998",
+            Content = "Invalid or Already Used Key.",
+            Image = "rbxassetid://89375684433942",
             Time = 5
         })
     end
@@ -87,9 +87,9 @@ local savedKey = LoadSavedKey()
 if savedKey then
     if IsKeyValid(savedKey, player) then
         OrionLib:MakeNotification({
-            Name = "Auto Validation",
-            Content = "Key Loaded and Validated Automatically.",
-            Image = "rbxassetid://4483345998",
+            Name = "Checking",
+            Content = "Key Loaded And Validated!",
+            Image = "rbxassetid://71378523145158",
             Time = 5
         })
 
@@ -98,8 +98,8 @@ if savedKey then
     else
         OrionLib:MakeNotification({
             Name = "Invalid Saved Key",
-            Content = "Saved Key is Invalid. Please Enter Again.",
-            Image = "rbxassetid://4483345998",
+            Content = "Get A New Key!",
+            Image = "rbxassetid://89375684433942",
             Time = 5
         })
     end
@@ -107,7 +107,7 @@ else
     OrionLib:MakeNotification({
         Name = "No Saved Key",
         Content = "Please Enter Your Key Manually.",
-        Image = "rbxassetid://4483345998",
+        Image = "rbxassetid://89375684433942",
         Time = 5
     })
 end
