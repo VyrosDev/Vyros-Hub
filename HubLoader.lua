ddb3d61e78cb2d752fc3ad7fd9b35e90a796bec0
Vyros Hub <alexandregomesap202@gmail.com>
2024-11-30 16:33:31 -0300

    HubLoader.lua

--- HubLoader.lua
+++ HubLoader.lua
@@ -6,6 +6,9 @@ local OrionLib = loadstring(game:HttpGet(('https://raw.githubusercontent.com/CkV
 local Player = game.Players.LocalPlayer
 local linkCopied = false -- Flag to track if the link was copied
 
+-- Store the UI instance to easily destroy it later
+local UIInstance = nil
+
 function CorrectKeyAndLoadTheScript()
     loadstring(game:HttpGet('https://raw.githubusercontent.com/CkVyros/Vyros-Hub/refs/heads/main/Test.lua'))() --This Where You Put The Script That Will Be Loaded If The Key Is Correct
     DestroyUI() -- Destroy the key system interface after the script loads
@@ -13,23 +16,23 @@ end
 
 -- Function to destroy the key system interface
 function DestroyUI()
-    for _, gui in pairs(game.CoreGui:GetChildren()) do
-        if gui.Name == "Vyros Hub | Key System" then
-            gui:Destroy()
-        end
+    if UIInstance and UIInstance.Parent then
+        UIInstance:Destroy()
     end
 end
 
 OrionLib:MakeNotification({
     Name = "Logged In!",
     Content = "Welcome, "..Player.Name..".",
-    Image = "rbxassetid://101023107339989",
+    Image = "rbxassetid://96062201354965",
     Time = 5
 }) -- Notification
 
 getgenv().Key = "ClarkyyxVyros" -- Set the correct key here
 getgenv().KeyInput = "string" -- Initialize KeyInput
 
+-- Create a new tab and section, and keep a reference to the main UI instance
+UIInstance = Window
 local Tab = Window:NewTab("KEY SYSTEM") -- Create a new tab
 local Section = Tab:NewSection("Key System") -- Create a section
 
@@ -37,12 +40,12 @@ Section:NewTextBox("Enter Key", "Key", function(Value)
     getgenv().KeyInput = Value
 end) 
 
-Section:NewButton("Check Key", "This Will Check The Key You Enter", function()
+Section:NewButton("Submit", "Check your key", function()
     if not linkCopied then
         OrionLib:MakeNotification({
             Name = "Error",
-            Content = "You must copy the link before checking the key.",
-            Image = "rbxassetid://4483345998",
+            Content = "Your key is invalid.",
+            Image = "rbxassetid://89375684433942",
             Time = 5
         })
         return
@@ -52,14 +55,14 @@ Section:NewButton("Check Key", "This Will Check The Key You Enter", function()
         OrionLib:MakeNotification({
             Name = "Checking Key",
             Content = "Checking the key you entered...",
-            Image = "rbxassetid://4483345998",
+            Image = "rbxassetid://101023107339989",
             Time = 5
         })
         wait(2)
         OrionLib:MakeNotification({
             Name = "Correct Key!",
             Content = "The key you entered is valid.",
-            Image = "rbxassetid://4483345998",
+            Image = "rbxassetid://71378523145158",
             Time = 5
         })
         wait(0.3)
@@ -68,26 +71,26 @@ Section:NewButton("Check Key", "This Will Check The Key You Enter", function()
         OrionLib:MakeNotification({
             Name = "Checking Key",
             Content = "Checking the key you entered...",
-            Image = "rbxassetid://4483345998",
+            Image = "rbxassetid://101023107339989",
             Time = 5
         })
         wait(2)
         OrionLib:MakeNotification({
             Name = "Incorrect Key!",
             Content = "The key you entered is invalid.",
-            Image = "rbxassetid://4483345998",
+            Image = "rbxassetid://89375684433942",
             Time = 5
         })
     end
 end)  
 
-Section:NewButton("Copy Link", "Copy the required link", function()
+Section:NewButton("Get Key", "Copy the required link", function()
     setclipboard("Link here") -- Put your link here
     linkCopied = true -- Set the flag to true
     OrionLib:MakeNotification({
         Name = "Copied Link!",
         Content = "Successfully copied the link.",
-        Image = "rbxassetid://4483345998",
+        Image = "rbxassetid://71378523145158",
         Time = 5
     })
 end)
